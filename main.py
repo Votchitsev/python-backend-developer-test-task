934a0071d0b5f060bdbc9a7ad73b6f0a4185c74a
Dmitry <dmitry.votchitsev@gmail.com>
2023-03-27 13:04:22 +0200

    asynchrony added

--- main.py
+++ main.py
@@ -1,42 +1,69 @@
-from pprint import pprint
 import os
-import requests
 import base64
+import asyncio
+import time
+from aiohttp import ClientSession
 
 
-def fetchData(url, path=''):
+async def fetchData(url, dir='', path=''):
+    if (dir):
+        try:
+            os.mkdir('result/' + dir)
+        except FileExistsError:
+            pass
+
+        repo_dir = dir
     
-    response = requests.get(
-        url,
-        headers = {
-        'accept': 'application/json',
-        } 
-        )
+    async with ClientSession() as session:
+        async with session.get(url, headers={'accept': 'application/json'}) as response:
 
-    response = response.json()
+            response = await response.json()
 
-    for file in response:
-        if file['type'] == 'file':
-            blob = requests.get(
-                f"https://gitea.radium.group/api/v1/repos/radium/project-configuration/git/blobs/{file['sha']}",
-                headers = {
-                    'accept': 'application/json',
-                }
-            )
+            for file in response:
+                if file['type'] == 'file':
+                    async with session.get(
+                        f"https://gitea.radium.group/api/v1/repos/radium/project-configuration/git/blobs/{file['sha']}",
+                        headers = {
+                            'accept': 'application/json',
+                        }
+                    ) as blob:
 
-            blob = blob.json()
+                        blob = await blob.json()
 
-            d = blob['content'].encode('utf-8')
-            d = base64.decodebytes(d)
+                        d = blob['content'].encode('utf-8')
+                        d = base64.decodebytes(d)
 
-            f = open(str('./result/' + f"{path}/" + file['name']), 'wb')
-            f.write(d)
-            f.close()
+                        f = open(str(f"./result/{repo_dir}/" + f"{path}/" + file['name']), 'wb')
+                        f.write(d)
+                        f.close()
 
-        if file['type'] == 'dir':
-            os.mkdir(f"./result/{file['path']}")
-            fetchData(file['url'], file['path'])
+                if file['type'] == 'dir':
+                    try:
+                        os.mkdir(f"./result/{repo_dir}/{file['path']}")
+                        await fetchData(file['url'], repo_dir, file['path'])
+
+                    except FileExistsError:
+                        print(f"Файл {file['path']} существует.")
 
 
+if __name__ == '__main__':
 
-fetchData('https://gitea.radium.group/api/v1/repos/radium/project-configuration/contents/%2F?ref=HEAD')
+    async def main():
+
+        url = 'https://gitea.radium.group/api/v1/repos/radium/project-configuration/contents/%2F?ref=HEAD'
+
+        task1 = asyncio.create_task(fetchData(url, 'task1'))
+        task2 = asyncio.create_task(fetchData(url, 'task2'))
+        task3 = asyncio.create_task(fetchData(url, 'task3'))
+
+        start = time.time()
+
+        print('Старт ->')
+
+        await task1
+        await task2
+        await task3
+
+        print('время выполнения скрипта: ', time.time() - start)
+
+    asyncio.run(main())
